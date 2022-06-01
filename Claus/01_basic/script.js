3303fd95416ec5ed2abd0c31d910e8a6aea93257
OinkoIT <claus.mersch@gmail.com>
2022-06-01 10:18:48 +0200

    create structure

---
+++ Claus/01_basic/script.js
@@ -0,0 +1,5 @@
+/* Ex 1
+
+Create a form with input fields for your firstname, lastname and age.  Extract the values from each input field and display them all in a separate div.
+
+If the length of your name is larger than 5 characters, change the text colour to green. Otherwise, change the text-colour to red. */
